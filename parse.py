e31b8e4224eb84a0fe4c6d0ef51a7afc5eb2791e
Ben West <Xodarap@users.noreply.github.com>
2020-12-03 19:25:03 -0800

    Update parse.py

--- parse.py
+++ parse.py
@@ -2,17 +2,22 @@ from bs4 import BeautifulSoup
 import re
 from pathlib import Path
 
+output_file = open("output4.csv", "w", encoding='utf-8')
+
 def parse_file(filename):
   f = open(filename, "r", encoding='utf-8')
-  printable_name = filename.split('- ')[1][0:-5]
+  printable_name = ('- '.join(filename.split('- ')[1:]))[0:-5]
   html_text = f.read()
   soup = BeautifulSoup(html_text, 'html.parser')
   headers = soup.select('h2.gmql0nx0.l94mrbxd.p1ri9a11.lzcic4wl.aahdfvyu.hzawbc8m')
   donation_strings = [(header.text.split(' donated ')) for header in headers]
   for donation in donation_strings:
-    printable = [v.encode("utf-8").decode('utf-8').replace(',','') for v in donation]
-    # todo: there's still some weird stuff with UTF encoding here
-    print( printable_name.replace(',','') +',' + ','.join(printable).encode("utf-8").decode('utf-8')[0:-1])
+    printable = [v.replace(',','') for v in donation]
+    single_line =  ','.join(printable).encode("utf-8").decode('utf-8')[0:-1]
+    if '$' in single_line:   # without a $ it's a title and not a donation
+      output_file.write( printable_name.replace(',','') +',' + single_line +'\n')
 
 for path in Path('2020-12-03-20201204T021819Z-001/2020-12-03').rglob('*.html'):
   parse_file('2020-12-03-20201204T021819Z-001/2020-12-03/' + path.name)
+
+output_file.close()
