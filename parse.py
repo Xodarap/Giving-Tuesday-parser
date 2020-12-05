c09e46c5462567109fd9a04cbcc8be371d6322a2
Ben West <Xodarap@users.noreply.github.com>
2020-12-05 12:48:09 -0800

    handle dates

--- parse.py
+++ parse.py
@@ -2,22 +2,43 @@ from bs4 import BeautifulSoup
 import re
 from pathlib import Path
 
-output_file = open("output4.csv", "w", encoding='utf-8')
+output_file = open("output_dates.csv", "w", encoding='utf-8')
 
 def parse_file(filename):
   f = open(filename, "r", encoding='utf-8')
   printable_name = ('- '.join(filename.split('- ')[1:]))[0:-5]
   html_text = f.read()
   soup = BeautifulSoup(html_text, 'html.parser')
-  headers = soup.select('h2.gmql0nx0.l94mrbxd.p1ri9a11.lzcic4wl.aahdfvyu.hzawbc8m')
-  donation_strings = [(header.text.split(' donated ')) for header in headers]
+  donation_strings = [handle_donation(d) for d in soup.select('div.j83agx80.cbu4d94t.ew0dbk1b.irj2b8pg')]
   for donation in donation_strings:
-    printable = [v.replace(',','') for v in donation]
-    single_line =  ','.join(printable).encode("utf-8").decode('utf-8')[0:-1]
-    if '$' in single_line:   # without a $ it's a title and not a donation
-      output_file.write( printable_name.replace(',','') +',' + single_line +'\n')
+    if '$' in donation:   # without a $ it's a title and not a donation
+      combined = printable_name.replace(',','') +',' + donation
+      output_file.write(combined +'\n')
+      # print( combined )
 
-for path in Path('2020-12-03-20201204T021819Z-001/2020-12-03').rglob('*.html'):
-  parse_file('2020-12-03-20201204T021819Z-001/2020-12-03/' + path.name)
+def handle_donation(donation):
+  headers = donation.select('h2.gmql0nx0.l94mrbxd.p1ri9a11.lzcic4wl.aahdfvyu.hzawbc8m')
+  if len( headers) == 0:
+    return ''
+  donation_strings = headers[0].text.split(' donated ')
+  printable = [v.replace(',','') for v in donation_strings]
+  single_line =  ','.join(printable).encode("utf-8").decode('utf-8')[0:-1]
+  return single_line + ',' + get_date(donation)
 
+def get_date(donation):
+  date_elements = donation.select('span.b6zbclly span.b6zbclly')
+  if date_elements is None:
+    return ''
+  visible = list(filter(lambda v: not (v.has_attr('style')), date_elements))
+  combined = ''.join([d.text for d in visible])
+  if combined[0:2] == 'ec':
+    return 'D' + combined
+  elif combined[0:2] == 'es':
+    return 'Y' + combined
+  return combined
+
+for path in Path('new/2020-12-03').rglob('*.html'):
+  parse_file('new/2020-12-03/' + path.name)
+  print( path.name )
+# parse_file('new/2020-12-03/Giving Tuesday 2020- Albert Schweitzer Foundation via ACE.html')
 output_file.close()
